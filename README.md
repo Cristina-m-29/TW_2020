851815f9422f2dc7707edc518fbd5d23c25747b9
MadalinaPlugariu <61787426+MadalinaPlugariu@users.noreply.github.com>
2020-05-04 19:49:39 +0300

    Finished pages

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # TW_2020
 Proiect TW 2020 - Mititelu Ioana-Cristina si Plugariu Madalina-Gabriela
-
-Link trello: https://trello.com/b/ZcvBo44v/tw2020
---
+++ js/categorii/women.js
@@ -0,0 +1,74 @@
+var first=document.getElementsByClassName('first')[0];
+var first_img=document.getElementsByClassName('first_img')[0];
+var first_txt=document.getElementsByClassName('first_txt')[0];
+
+var second=document.getElementsByClassName('second')[0];
+var second_img=document.getElementsByClassName('second_img')[0];
+var second_txt=document.getElementsByClassName('second_txt')[0];
+
+var third=document.getElementsByClassName('third')[0];
+var third_img=document.getElementsByClassName('third_img')[0];
+var third_txt=document.getElementsByClassName('third_txt')[0];
+
+var fourth=document.getElementsByClassName('fourth')[0];
+var fourth_img=document.getElementsByClassName('fourth_img')[0];
+var fourth_txt=document.getElementsByClassName('fourth_txt')[0];
+
+first.onmouseover=first.onmouseout=firstOpacity;
+second.onmouseover=second.onmouseout=secondOpacity;
+third.onmouseover=third.onmouseout=thirdOpacity;
+fourth.onmouseover=fourth.onmouseout=fourthOpacity;
+
+
+function firstOpacity(event){
+    if(event.type == 'mouseover')
+    {
+       first_img.style.opacity='0.5';
+       first_txt.style.opacity='0.5';
+      
+    }
+    if(event.type == 'mouseout'){
+        first_img.style.opacity="1";
+        first_txt.style.opacity="1";
+      
+    }
+    
+
+}
+
+function secondOpacity(event){
+    if(event.type == 'mouseover')
+    {
+       second_txt.style.opacity='0.5';
+       second_img.style.opacity='0.5';
+    }
+    if(event.type == 'mouseout'){
+        second_txt.style.opacity="1";
+        second_img.style.opacity="1";
+    }
+}
+
+function thirdOpacity(event){
+    if(event.type == 'mouseover')
+    {
+       third_img.style.opacity='0.5';
+       third_txt.style.opacity='0.5';
+    }
+    if(event.type == 'mouseout'){
+        third_img.style.opacity="1";
+        third_txt.style.opacity="1";
+    }
+}
+
+function fourthOpacity(event){
+    if(event.type == 'mouseover')
+    {
+       fourth_img.style.opacity='0.5';
+       fourth_txt.style.opacity='0.5';
+    }
+    if(event.type == 'mouseout'){
+        fourth_img.style.opacity="1";
+        fourth_txt.style.opacity="1";
+    }
+}
+
